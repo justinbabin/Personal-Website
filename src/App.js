8fd498ff39c87066849b018dcc6597ed99fe3004
justinbabin <justin.g.babin@gmail.com>
2023-06-05 13:38:09 -0700

    adding files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// src/App.js
+
+import React from "react";
+
+export default function App() {
+    return (
+        <main>
+            <Navbar></Navbar>
+            <About></About>
+            <Projects></Projects>
+            <Skills></Skills>
+            <Testimonials></Testimonials>
+            <Contact></Contact>
+        </main>
+    );
+}
